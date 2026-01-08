6dcf3fc491490b22abda14d62680306322bba7d0
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-01-08 13:43:14 +0900

    Add main function to estimate π with 10 million samples

--- CalcPi.jl/main.jl
+++ CalcPi.jl/main.jl
@@ -1,1 +1,12 @@
 using CalcPi
+
+# Example entry point
+
+function main()
+    num_samples = 10_000_000
+    println("Estimating π using $num_samples samples…")
+    pi_estimate = calc_pi(UInt64(num_samples))
+    println("Estimated π value: $pi_estimate")
+end
+
+main()
